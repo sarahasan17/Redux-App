ccfbbca3650bddbbb075ad9914eedcdb4f9fc910
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-09-28 11:44:38 +0530

    action reducer

---
+++ src/state/reducers/amountReducer.js
@@ -0,0 +1,8 @@
+export default reducer=(state=0,action)=>{
+    if(action.type==='deposit'){
+        return state + action.payload;
+    }
+    else if(action.type==='withdraw'){
+        return state - action.payload;
+    }
+}
